4f4f87d8f49e461265069f1262dcc07cf07ef2c8
kbkk <6276426+kbkk@users.noreply.github.com>
2020-07-15 23:56:56 +0200

    feature: Add Server impl

---
+++ src/Server.ts
@@ -0,0 +1,114 @@
+import * as http from 'http';
+import {once} from 'events';
+import {AddressInfo} from "net";
+import {Router} from "./Router";
+
+export type RequestContext = {
+    readonly method: string;
+    res?: string | Buffer;
+    status?: number;
+};
+
+export type BoundMiddlewareCallback = () => Promise<unknown>;
+
+export type MiddlewareCallback = (ctx: RequestContext, next: BoundMiddlewareCallback) => Promise<unknown> | unknown;
+
+const noop: BoundMiddlewareCallback = () => Promise.resolve();
+
+class Server {
+    private _router = new Router<MiddlewareCallback>();
+    private _httpServer: http.Server | undefined;
+
+    public use(path: string, fn: MiddlewareCallback) {
+        this._router.register(path, fn);
+    }
+
+    public get(path: string, fn: MiddlewareCallback) {
+        this._router.register(path, function getMiddleware(ctx, next) {
+            if (ctx.method === 'GET') {
+                ctx.status = 200;
+                return fn(ctx, next);
+            } else {
+                return next();
+            }
+        });
+    }
+
+    public post(path: string, fn: MiddlewareCallback) {
+        this._router.register(path, function postMiddleware(ctx, next) {
+            if (ctx.method === 'POST') {
+                ctx.status = 200;
+                return fn(ctx, next);
+            } else {
+                return next();
+            }
+        });
+    }
+
+    public async start(port: number, hostname?: string): Promise<string> {
+        if (this._httpServer) {
+            throw new Error('The server has already been started!');
+        }
+
+        this._httpServer = http.createServer(this._handleRequest.bind(this));
+
+        await once(
+            this._httpServer.listen(port, hostname),
+            'listening'
+        );
+
+        const address = this._httpServer.address() as AddressInfo;
+
+        return `http://${address.address}:${address.port}/`;
+    }
+
+    public async stop(): Promise<void> {
+        if (!this._httpServer) {
+            throw new Error('The server is not running!');
+        }
+
+        await once(
+            this._httpServer?.close() as http.Server,
+            'close'
+        );
+
+        this._httpServer = undefined;
+    }
+
+    private async _handleRequest(req: http.IncomingMessage, res: http.ServerResponse) {
+        const chunks: Buffer[] = [];
+
+        for await (const chunk of req) {
+            chunks.push(chunk);
+        }
+
+        const middlewareStack = this._router.resolve(req.url!);
+
+        if (middlewareStack.length === 0) {
+            res.writeHead(404);
+            res.end();
+
+            return;
+        }
+
+        const requestContext = {
+            method: req.method,
+            res: '',
+            status: 405
+        };
+
+        let nextMiddleware: BoundMiddlewareCallback = noop;
+
+        for (let i = middlewareStack.length - 1; i >= 0; i--) {
+            nextMiddleware = middlewareStack[i].bind(null, requestContext, nextMiddleware);
+        }
+
+        await nextMiddleware?.();
+
+        res.writeHead(requestContext.status);
+        res.write(requestContext.res);
+        res.end();
+    }
+}
+
+export default Server;
---
+++ src/__tests__/Server.ts
@@ -0,0 +1,87 @@
+import * as chai from "chai";
+import * as chaiAsPromised from "chai-as-promised";
+import fetch from "node-fetch";
+import Server from "../Server";
+
+chai.use(chaiAsPromised);
+const {expect} = chai;
+
+describe('Server', () => {
+    let server: Server;
+    let serverUrl: string;
+
+    beforeEach(async () => {
+        server = new Server();
+
+        serverUrl = await server.start(0, '127.0.0.1');
+    });
+
+    afterEach(async () => {
+        await server.stop();
+    });
+
+    it('should throw if start() is called after the server has already been started', async () => {
+        await expect(server.start(0)).to.eventually.be.rejectedWith('The server has already been started!');
+    });
+
+    it('should respond with 404 when no route is found', async () => {
+        const result = await fetch(`${serverUrl}test`);
+
+        expect(result.status).to.eql(404);
+    });
+
+    it('should respond with 405 when a route is invoked with invalid HTTP method', async () => {
+        server.post('/test', async req => {
+            req.res = 'POST /test response'
+        });
+
+        const result = await fetch(`${serverUrl}test`);
+
+        expect(result.status).to.eql(405);
+    });
+
+    it('should handle GET requests', async () => {
+        server.get('/test', async req => {
+            req.res = 'GET /test response'
+        });
+
+        const result = await fetch(`${serverUrl}test`);
+
+        expect(result.status).to.eql(200);
+        expect(await result.text()).to.eql('GET /test response');
+    });
+
+    it('should handle POST requests', async () => {
+        server.post('/test', async req => {
+            req.res = 'POST /test response'
+        });
+
+        const result = await fetch(`${serverUrl}test`, {method: 'POST'});
+
+        expect(result.status).to.eql(200);
+        expect(await result.text()).to.eql('POST /test response');
+    });
+
+    it('should handle different GET/POST callbacks on same path', async () => {
+        server.use('/same-path-test', async (req, next) => {
+            req.res = 'middleware ';
+
+            await next();
+        });
+
+        server.get('/same-path-test', async req => {
+            req.res += 'GET /same-path-test response';
+        });
+
+        server.post('/same-path-test', async req => {
+            req.res += 'POST /same-path-test response';
+        });
+
+        const getResult = await fetch(`${serverUrl}same-path-test`);
+        const postResult = await fetch(`${serverUrl}same-path-test`, {method: 'POST'});
+
+        expect(await getResult.text()).to.eql('middleware GET /same-path-test response');
+        expect(await postResult.text()).to.eql('middleware POST /same-path-test response');
+    });
+});
+
